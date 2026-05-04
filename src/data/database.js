1c6a40f777db15189741d541ff6a748d524a8f7a
henrik-lund <henkelund@live.se>
2026-05-04 16:48:10 +0200

    first commit

---
+++ src/data/database.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDmMsW84_Z79uxBVKlg_g7VSeH99n-xxYk",
+	authDomain: "yippi-webshop.firebaseapp.com",
+	projectId: "yippi-webshop",
+	storageBucket: "yippi-webshop.firebasestorage.app",
+	messagingSenderId: "1082684000518",
+	appId: "1:1082684000518:web:bd2460cec62dc40e4d7687"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
